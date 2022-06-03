faee1fc3ebb696efef91c442b86196691318cfd8
Denis <dsrgvv0@gmail.com>
2022-06-03 09:29:03 +0300

    added font

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <p>work</p>
+      </header>
     </div>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import { store } from './Redux/store'
+import { createStore } from './Redux/store'
 import App from './Components/App/App'
-import './index.css'
+import './scss/index.scss'
 
+const store = createStore()
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
