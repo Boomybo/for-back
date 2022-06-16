450c3ad664a4ffcb84e873210cd42cd0f096e07a
Denis Sergeev <dsrgvv0@gmail.com>
2022-06-16 07:47:43 +0000

    create showcase & fix bugs

--- src/Components/Products/CardItem.jsx
+++ src/Components/Products/CardItem.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { PlusOutlined } from '@ant-design/icons'
 
+import img from '../../assets/imgTest.jpeg'
+
 import styles from './Products.module.scss'
 
 const CardItem = ({ title, descr, price }) => {
   return (
     <div className={styles.card}>
-      <img
-        alt="example"
-        src="https://89.img.avito.st/image/1/1.QBjw4ba27PHmQVbx_JhvbSNC7PtO4u1LRELu.9RjV_laMFNM_G6y2KkxCh6SP4T8yHEDYGlYXKl0Fhzs"
-      />
+      <img alt="example" src={img} />
       <div className={styles.card_body}>
         <div className={styles.title}>{title}</div>
         <div className={styles.descr}>{descr}</div>
